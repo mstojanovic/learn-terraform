eabda8d63351341dba81e3b7b68fbb8a06280697
Marko Stojanovic <mstojano@proton.me>
2024-11-24 23:48:07 +0100

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "terraform-learn-ms-2024"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
